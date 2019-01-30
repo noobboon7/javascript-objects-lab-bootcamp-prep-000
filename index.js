107d6ae9118351c9f357d8c5b6208f161f081330
Jerry Velazquez <jvr572@gmail.com>
2019-01-30 16:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var object = {};
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  new Object({}, object, {[key]:value});
+  new Object({}, object, {[key]:"value"});
   return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  new Object({}, object, {[key]:"value"});
+  return object;
 }
+function 
