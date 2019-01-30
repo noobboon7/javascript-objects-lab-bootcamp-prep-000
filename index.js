9418db613247a124aa9f607be8d2d3207adbd336
Jerry Velazquez <jvr572@gmail.com>
2019-01-30 16:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function updateObjectWithKeyAndValue(object, key, value){
   
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  new Object({}, object, {[key]:value});
+  new Object(object, {[key]:value});
   return object;
 }
 function deleteFromObjectByKey(object, key){
-  Object.assign({}, object, {key});
+  Object.assign( object, {key});
   return object;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
